abfa5455935db6f920a2e7ae37b56a1b49251247
Olle Bergkvist <ollebergkvist@gmail.com>
2021-03-03 07:20:04 -0700

    Added TCP files

---
+++ TCPreceiver.py
@@ -0,0 +1,36 @@
+# UDP receiver
+# Olle Bergkvist & August M Rosenqvist
+
+from socket import *
+
+serverPort = 12000
+counter = 10000
+
+# Create UDP socket and bind to specified port
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('', serverPort))
+
+# Activate listen on socket
+serverSocket.listen(1)
+
+print ("The TCP receiver is ready to receive.\n")
+
+while True:
+    # Read client's message and remember client's address (IP and port)
+    counter += 1
+    
+    # server waits for incoming requests; new socket created on return
+    connectionSocket, addr = serverSocket.accept() //
+
+    # read sentence of bytes from socket sent by the client
+    message = connectionSocket.recv(1024).decode()
+
+    messageArray = message.split(";")
+    sequenceNumber = int(messageArray[0])
+    extractedMessage = messageArray[1]
+
+    if counter != sequenceNumber:
+        print("Wrong package order detected!")
+        print("Expected ID:", counter, "Received ID:", sequenceNumber)
+    else:
+        print("Correct package received:", sequenceNumber)
---
+++ TCPsender.py
@@ -0,0 +1,46 @@
+# UDP sender
+# Olle Bergkvist & August M Rosenqvist
+
+from socket import *
+import time
+
+serverName = '192.168.2.4'
+serverPort = 12000
+sequenceNumber = 10000
+sequenceNumberEnd = ";"
+streamingData = 'A'*1400
+endCharacters = "####"
+compiledMessage = ""
+
+# create TCP socket on client to use for connecting to remote server
+clientSocket = socket(AF_INET, SOCK_STREAM)
+
+# open the TCP connection
+clientSocket.connect((serverName, serverPort))
+
+print("Sending TCP packages to:", serverName, "\nPress Ctrl+C to stop.\n")
+
+# Loop until keyboard interrupt (Ctrl+C)
+try:
+    while True:
+        # Increment sequence number
+        sequenceNumber += 1
+
+        # Concatenate message
+        compiledMessage = str(sequenceNumber) + sequenceNumberEnd + streamingData + endCharacters
+
+        # Send sentence to socket; server and port number required
+        # Need to convert message from string to bytes for Python 3
+        clientSocket.send(compiledMessage.encode(), (serverName, serverPort))
+
+        # Display current package ID
+        print("\rCurrent package:", sequenceNumber, end='', flush=True)
+
+        # Wait one second before sending next package
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    print("Stopped.")
+    # Close socket
+    clientSocket.close()
+    time.sleep(1)
